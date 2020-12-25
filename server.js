a79c38566c0cf5af3518037171abcf458ab918ba
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-12-25 07:34:50 +0545

    refactored the file structure to get follow MVC architecture

--- server.js
+++ server.js
@@ -1,90 +1,18 @@
-const fs = require('fs');
-
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 app.use(morgan('dev'));
 app.use(express.json());
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-const getAllTours = (req, res) => {
-  res
-    .status(200)
-    .json({ status: 'success', results: tours.length, data: { tours } });
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      console.log(err);
-    }
-  );
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTour,
-    },
-  });
-};
-
-const updateTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  if (tour) {
-    return res
-      .status(200)
-      .json({ status: 'success', msg: 'Updated the course' });
-  }
-  res.status(404).json({
-    status: 'failed',
-    data: { msg: `Tour with the given id: ${id} is invalid` },
-  });
-};
-
-const getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  if (tour) {
-    return res
-      .status(200)
-      .json({ status: 'success', results: tours.length, data: { tour } });
-  }
-  res.status(404).json({
-    status: 'failed',
-    data: { msg: `Invalid ID` },
-  });
-};
-
-const deleteTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  if (tour) {
-    return res.status(204).json({ status: 'success', data: null });
-  }
-  res.status(404).json({
-    status: 'failed',
-    data: { msg: `Invalid ID` },
-  });
-};
-
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
 const PORT = 3000;
+
+app.use('/api/v1/users', userRouter);
+
+app.use('/api/v1/tours', tourRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
