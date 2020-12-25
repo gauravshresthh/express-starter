17f026ac6f2f6b33fd252f4d3ce2f69976c56118
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-12-25 07:45:22 +0545

    server and app extracted into two different files for separation of concerns

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,17 +1,6 @@
-const express = require('express');
-const morgan = require('morgan');
-const app = express();
-
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-
-app.use(morgan('dev'));
-app.use(express.json());
+const app = require('./app');
 
 const PORT = 3000;
-
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/tours', tourRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
