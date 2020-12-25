d36253ca1fcc14d87cea251a7a275ebe2739bed2
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-12-25 07:12:51 +0545

    used route and chained all the methods for nicer looking code

--- server.js
+++ server.js
@@ -76,16 +76,13 @@ const deleteTour = (req, res) => {
   });
 };
 
-app.get('/api/v1/tours', getAllTours);
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
-app.post('/api/v1/tours', createTour);
-
-app.patch('/api/v1/tours/:id', updateTour);
-
-app.get('/api/v1/tours/:id', getTour);
-
-app.delete('/api/v1/tours/:id', deleteTour);
-
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 const PORT = 3000;
 
 app.listen(PORT, () => {
